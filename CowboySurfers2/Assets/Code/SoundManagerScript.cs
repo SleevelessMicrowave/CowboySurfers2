c051afd49cdc184f0c43aff4c5bc2d0a85f30858
K1tosen <crazymonkryman@gmail.com>
2020-04-27 15:36:21 -0400

    v1
    added background music during main gameplay loop
    
    added sound effects for jump and roll

---
+++ CowboySurfers2/Assets/Code/SoundManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+    public static AudioClip jump,slide;
+    static AudioSource audioSrc;
+	// Use this for initialization
+	void Start () {
+        jump = Resources.Load<AudioClip>("AlternativeJumpSound");
+        slide = Resources.Load<AudioClip>("slide");
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static void playSound (string clip)
+    {
+        switch (clip)
+        {
+            case "AlternativeJumpSound":
+                audioSrc.PlayOneShot(jump);
+                break;
+            case "slide":
+                audioSrc.PlayOneShot(slide);
+                break;
+       
+        }
+    }
+}
