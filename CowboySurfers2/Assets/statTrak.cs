8e3c5ddab50c7891eb0d5411632f6d1924e628be
K1tosen <crazymonkryman@gmail.com>
2020-04-28 19:48:38 -0400

    made intro screen
    intro screen
    
    +graphics
    +play button switches scene to game
    +high scores

---
+++ CowboySurfers2/Assets/statTrak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class statTrak : MonoBehaviour {
+
+    public Text maxBottles;
+    public Text maxTime;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        maxBottles.text = "Most Bottles Collected: " + GM.greatestBottle;
+        maxTime.text = "Most Time Survived: " + GM.greatestTime;
+	}
+}
