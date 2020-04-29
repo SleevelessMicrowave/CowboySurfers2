4b31881588cc88fe7183c15f267ca19131d19425
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-04-29 18:50:07 -0400

    Fix Beginning Animation

---
+++ CowboySurfers2/Assets/animationTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class animationTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(videoLength());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator videoLength()
+    {
+
+        yield return new WaitForSeconds(48);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
