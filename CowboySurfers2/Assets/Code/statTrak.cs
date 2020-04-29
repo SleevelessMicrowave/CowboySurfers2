23e25622bd9bf96aac79a53031ffb2955491d0f6
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2020-04-29 18:01:27 -0400

    Fix End Screen Errors

--- CowboySurfers2/Assets/Code/statTrak.cs
+++ CowboySurfers2/Assets/Code/statTrak.cs
@@ -18,7 +18,7 @@ public class statTrak : MonoBehaviour {
 	void Update () {
         maxBottles.text = "Most Bottles Collected: " + GM.greatestBottle;
         maxTime.text = "Most Time Survived: " + GM.greatestTime;
-        bottles.text = "Bottles Collected: " + GM.tmpcoinTotal;
-        time.text = "Time Survived: " + GM.tmptimeTotal;
+        //bottles.text = "Bottles Collected: " + GM.coinTotal;
+        //time.text = "Time Survived: " + GM.timeTotal;
     }
 }
